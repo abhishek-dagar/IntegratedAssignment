4e69c8607f01135c7f7ca1bf7dfa15e316298f23
Abhishek Dagar <59116285+abhishek-dagar@users.noreply.github.com>
2022-09-29 14:19:17 +0530

    Delete IManufacturer_ToTrainee directory

--- IManufacturer_ToTrainee/src/main/java/com/infy/dto/ManufacturingUnit.java
+++
@@ -1,5 +0,0 @@
-package com.infy.dto;
-
-public enum ManufacturingUnit {
-	U001, U002, U003, U004, U005
-}
--- IManufacturer_ToTrainee/src/main/java/com/infy/exception/InfyEmployeeException.java
+++
@@ -1,9 +0,0 @@
-package com.infy.exception;
-
-public class InfyEmployeeException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public InfyEmployeeException(String message) {
-		super(message);
-	}
-}
